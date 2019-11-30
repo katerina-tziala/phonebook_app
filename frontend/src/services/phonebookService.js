df51bed3af0f98bd491c78978cbff2172662450f
katerina-tziala <katerina.tziala@gmail.com>
2019-11-30 10:50:37 +0100

    Update phonebookService.js

--- frontend/src/services/phonebookService.js
+++ frontend/src/services/phonebookService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const baseUrl = '/api/persons';
-
+const baseUrl = 'https://phonebook-app-kt.herokuapp.com/api/persons';
+//const baseUrl = 'http://localhost:3001/api/persons';
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then(response => response.data);
