46adf00010a01af1b007d5a0a9f3e78764d3948c
katerina-tziala <katerina.tziala@gmail.com>
2019-11-30 08:51:27 +0100

    connect front end to backend

--- frontend/src/services/phonebookService.js
+++ frontend/src/services/phonebookService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://phonebook-app-kt.herokuapp.com/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
