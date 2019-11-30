62f566e400368177beab7fc2be330a1adbf1e40b
katerina-tziala <katerina.tziala@gmail.com>
2019-11-30 19:42:04 +0100

    update backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Person = require('./models/person');
 const PORT = process.env.PORT || 3001;
-
+const DB_URL = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('build'));
 app.use(morgan((tokens, req, res) => {
     const stringParts = [];
     const method = tokens.method(req, res);
@@ -23,98 +25,167 @@ app.use(morgan((tokens, req, res) => {
         stringParts.push(JSON.stringify(req.body));
     }
     return stringParts.join(' ');
-  }));
+}));
 
-const generateId = () => {
-    const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0;
-    return maxId + 1
+const establishConnection = () => {
+    return mongoose.connect(DB_URL,
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+};
+
+const closeDBConnection = () => {
+    mongoose.connection.close();
 };
 
 const isBlank = (str) => !str || !str.length || /^\s*$/.test(str);
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    },
-    {
-        "name": "Ian Smith",
-        "number": "39-23-6342357",
-        "id": 5
-    }
-];
+const returnBadRequestError = (response, errorMessage) => {
+    return response.status(400).json({
+        error: errorMessage
+    });
+};
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
-});
-
-app.get('/api/info', (req, res) => {
-    const response = `<div><p>Phonebook has info for ${persons.length} persons</p><p>${new Date().toString()}</p></div>`
-    res.send(response);
-});
-
-
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
-});
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id);
-    response.status(204).end();
-});
-
-app.post('/api/persons/', (request, response) => {
-    const body = request.body;
-
-    if (isBlank(body.name)) {
-        return response.status(400).json({
-            error: 'name is missing'
+app.get('/api/info', (req, response) => {
+    return establishConnection().then(() => {
+        return Person.find({}).then(persons => {
+            closeDBConnection();
+            const info = `<div><p>Phonebook has info for ${persons.length} persons</p><p>${new Date().toString()}</p></div>`;
+            response.send(info);
         });
+    }).catch((error) => {
+        error.name = "DBConnectionError";
+        return next(error);
+    });
+});
+
+app.get('/api/persons', (request, response, next) => {
+    return establishConnection().then(() => {
+        return Person.find({}).then(persons => {
+            closeDBConnection();
+            return response.json(persons.map(person => person.toJSON()));
+        });
+    }).catch((error) => {
+        error.name = "DBConnectionError";
+        return next(error);
+    });
+});
+
+app.get('/api/persons/:id', (request, response, next) => {
+    return establishConnection().then(() => {
+        return Person.findById(request.params.id).then(person => {
+            closeDBConnection();
+            if (person) {
+                return response.json(person.toJSON());
+            } else {
+                return response.status(404).end();
+            }
+        }).catch(error => {
+            closeDBConnection();
+            return next(error);
+        });
+    }).catch((error) => {
+        error.name = "DBConnectionError";
+        return next(error);
+    });
+});
+
+
+app.post('/api/persons/', (request, response, next) => {
+    const body = request.body;
+    if (isBlank(body.name)) {
+        return returnBadRequestError(response, 'name is missing');
     }
     if (isBlank(body.number)) {
-        return response.status(400).json({
-            error: 'number is missing'
-        });
+        return returnBadRequestError(response, 'number is missing');
     }
-    if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        });
-    }
-    const person = {
+
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId(),
-    };
+    });
 
-    persons = persons.concat(person);
-
-    response.json(person);
+    return establishConnection().then(() => {
+        return person.save().then(addedPerson => {
+            closeDBConnection();
+            return addedPerson.toJSON();
+        }).then(savedAndFormattedPerson => {
+            return response.json(savedAndFormattedPerson);
+        }).catch(error => {
+            return next(error);
+        });
+    }).catch((error) => {
+        error.name = "DBConnectionError";
+        return next(error);
+    });
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+        _id: request.params.id
+    });
+    return establishConnection().then(() => {
+        return Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedPerson => {
+            return updatedPerson.toJSON();
+        }).then(savedAndFormattedPerson => {
+            return response.json(savedAndFormattedPerson);
+        }).catch(error => {
+            return next(error);
+        });
+    }).catch((error) => {
+        error.name = "DBConnectionError";
+        return next(error);
+    });
+});
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    return establishConnection().then(() => {
+        return Person.findByIdAndRemove(request.params.id).then(() => {
+            return response.status(204).end();
+        }).catch(error => {
+            return next(error);
+        });
+    }).catch((error) => {
+        error.name = "DBConnectionError";
+        return next(error);
+    });
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+    if (error.name === 'DBConnectionError') {
+        return response.status(500).send({ error: 'connection to database refused' });
+    }
+    if (error.name === 'ValidationError') {
+        let message = `${error._message}!`;
+        Object.keys(error.errors).forEach(key => {
+            message += ` ${error.errors[key]}`;
+        });
+        return response.status(400).send({ error: message });
+    }
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+    if (error.name === 'MongoError' && error.codeName === 'ImmutableField') {
+        return response.status(500).send({ error: 'internal server error' });
+    }
+    next(error);
+};
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`)
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
+import Notification from './components/Notification';
 import phoneBookService from './services/phonebookService';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-  const [ newName, setNewName ] = useState('');
-  const [ newNumber, setNewNumber ] = useState('');
-  const [ filter, setFilter ] = useState('');
-  
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  const [notification, setNotification] = useState(undefined);
+
+  const notify = (notification) => {
+    setNotification(notification);
+    setTimeout(() => setNotification(undefined), 5000);
+  };
+
   useEffect(() => {
     phoneBookService.getAll().then(initialPersons => setPersons(initialPersons));
   }, [])
@@ -28,49 +35,83 @@ const App = () => {
     }
     return newPersonsList.sort((personA, personB) => (personName(personA) < personName(personB)) ? -1 : (personName(personA) > personName(personB)) ? 1 : 0);
   };
-  
-  const addPerson = (event) => {
-    event.preventDefault()
-    if (persons.some(person => person.name === newName)) {
-      if (window.confirm(`${newName} is already added to phonebook replace the old number with the new one?`)) { 
-        // const changedPerson = { ...persons.find(person => person.name === newName), number: newNumber };
-        // phoneBookService.update(changedPerson.id, changedPerson).then(updatePerson => {
-        //   setPersons([...persons].map(person => person.id !== updatePerson.id ? person : updatePerson));
-        //   setNewName('');
-        //   setNewNumber('');
-        // });
+
+  const savePerson = (event) => {
+    event.preventDefault();
+    const changedPerson = persons.find(person => person.name === newName);
+    if (changedPerson) {
+      if (window.confirm(`${changedPerson.name} is already added to phonebook replace the old number with the new one?`)) {
+        updatePerson(changedPerson);
       }
     } else {
-      const newPerson = {
-        name: newName,
-        number: newNumber
-      };
-      phoneBookService.create(newPerson).then(addedPerson => {
-        setPersons(persons.concat(addedPerson));
-        setNewName('');
-        setNewNumber('');
-      });
+      createPerson();
     }
   };
 
+  const getNotificationError = (message) => {
+    return {
+      type: "error",
+      message: message
+    };
+  };
+
+  const getNotificationSuccess = (message) => {
+    return {
+      type: "success",
+      message: message
+    };
+  };
+
+  const createPerson = () => {
+    const newPerson = {
+      name: newName.trim(),
+      number: newNumber.trim()
+    };
+    phoneBookService.create(newPerson).then(addedPerson => {
+      setPersons(persons.concat(addedPerson));
+      setNewName('');
+      setNewNumber('');
+      notify(getNotificationSuccess(`${addedPerson.name} was successfully added to PhoneBook!`));
+    }).catch(error => {
+      notify(getNotificationError(error.response.data.error));
+    });
+  };
+
+  const updatePerson = (changedPerson) => {
+    changedPerson.number = newNumber.trim();
+    phoneBookService.update(changedPerson.id, changedPerson).then(updatePerson => {
+      setPersons([...persons].map(person => person.id !== updatePerson.id ? person : updatePerson));
+      setNewName('');
+      setNewNumber('');
+      notify(getNotificationSuccess(`${updatePerson.name} was successfully updated!`));
+    }).catch(error => {
+      notify(getNotificationError(`${changedPerson.name} is already removed from PhoneBook!`));
+    });
+  };
+
   const deletePerson = id => {
-    if (window.confirm(`Do you really want to delete ${persons.find(person => person.id === id).name}?`)) { 
+    const personToDelete = persons.find(person => person.id === id);
+    if (window.confirm(`Do you really want to delete ${personToDelete.name}?`)) {
       phoneBookService.deletePerson(id).then(() => {
         setPersons([...persons].filter(person => person.id !== id));
+        notify(getNotificationSuccess(`${personToDelete.name} was successfully removed from PhoneBook!`));
+      }).catch(error => {
+        notify(getNotificationError(`This person is already removed from PhoneBook!`));
       });
     }
   }
 
   const formSubmissionIsDisabled = () => {
-   return (isBlank(newName) || isBlank(newNumber));
+    return (isBlank(newName) || isBlank(newNumber));
   };
 
   return (
     <>
       <h1>Phonebook</h1>
+      <Notification notification={notification} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h2>Add New</h2>
-      <PersonForm submission={addPerson} isDisabled={formSubmissionIsDisabled()} newName={newName} newNumber={newNumber} nameChange={handleNewNameChange} numberChange={handleNewNumberChange}/>
+      <PersonForm submission={savePerson} isDisabled={formSubmissionIsDisabled()} newName={newName} newNumber={newNumber} nameChange={handleNewNameChange} numberChange={handleNewNumberChange} />
       <h2>Numbers</h2>
       <Persons persons={filterPersons()} deletePerson={deletePerson} />
     </>
