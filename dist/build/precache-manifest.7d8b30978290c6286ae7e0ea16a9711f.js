b4e8f1b3663393b8289976526ea26ebef4dc4a8b
katerina-tziala <katerina.tziala@gmail.com>
2020-01-09 23:20:13 +0100

    update

---
+++ dist/build/precache-manifest.7d8b30978290c6286ae7e0ea16a9711f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d41422ab0b60ca2274d62224883d1fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07e8ed13b69d3cb68956",
+    "url": "/static/css/main.98b62682.chunk.css"
+  },
+  {
+    "revision": "5c4d40d8992f0b3e1a2d",
+    "url": "/static/js/2.77709985.chunk.js"
+  },
+  {
+    "revision": "07e8ed13b69d3cb68956",
+    "url": "/static/js/main.1178bf05.chunk.js"
+  },
+  {
+    "revision": "dc6855c4870ad91e5d66",
+    "url": "/static/js/runtime-main.90d3c0e4.js"
+  }
+]);
